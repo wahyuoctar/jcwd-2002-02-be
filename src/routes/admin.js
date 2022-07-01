92a858775b12db431db2342007affb59d8bc9e1d
wahyuoctar <wahyuoctar@gmail.com>
2022-07-01 11:28:08 +0700

    [P2-38] Admin Edit Product Feature

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,5 +1,6 @@
 const adminControllers = require("../controller/admin");
 const fileUploader = require("../lib/uploader");
+const { authorizedLoginAdmin } = require("../middleware/authorizeLoginAdmin");
 
 const router = require("express").Router();
 
@@ -20,6 +21,18 @@ router.post("/product-category", adminControllers.addProductCategory);
 // Get Product Category
 router.get("/product-category", adminControllers.getAllProductCategory);
 
+// Get Product
 router.get("/product", adminControllers.getProduct);
+
+// Edit Product
+router.patch(
+  "/product/:productId",
+  fileUploader({
+    destinationFolder: "product",
+    prefix: "PRODUCT",
+    fileType: "image",
+  }).array("product_image_file", 5),
+  adminControllers.editProduct
+);
 
 module.exports = router;
