579b17ff8396fba5900a34d7189168ae46774b98
wahyuoctar <wahyuoctar@gmail.com>
2022-07-05 13:49:09 +0700

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,10 @@ const authRoutes = require("./auth");
 const productRoutes = require("./product");
 const userRoutes = require("./user");
 const adminRoutes = require("./admin");
-const stockRoutes = require("./stock");
 
 module.exports = {
   authRoutes,
   productRoutes,
   userRoutes,
   adminRoutes,
-  stockRoutes,
 };
