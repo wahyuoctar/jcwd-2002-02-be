19632f017ea349563278b97060e00aa6abb44365
wahyuoctar <wahyuoctar@gmail.com>
2023-03-04 14:24:14 +0700

    UPDATE DB

--- src/config/database.js
+++ src/config/database.js
@@ -3,6 +3,7 @@ const mySqlConfig = {
   MYSQL_PASSWORD: process.env.MYSQL_PASSWORD,
   MYSQL_DB_NAME: process.env.MYSQL_DB_NAME,
   MYSQL_HOST: process.env.MYSQL_HOST,
+  MYSQL_PORT: process.env.MYSQL_PORT,
 };
 
 module.exports = mySqlConfig;
--- src/lib/sequelize.js
+++ src/lib/sequelize.js
@@ -1,43 +1,43 @@
-const { Sequelize } = require("sequelize");
-const mySqlConfig = require("../config/database");
+const { Sequelize } = require('sequelize');
+const mySqlConfig = require('../config/database');
 
 const sequelize = new Sequelize({
   username: mySqlConfig.MYSQL_USERNAME,
   password: mySqlConfig.MYSQL_PASSWORD,
   database: mySqlConfig.MYSQL_DB_NAME,
   host: mySqlConfig.MYSQL_HOST,
-  port: 3306,
-  dialect: "mysql",
+  port: mySqlConfig.MYSQL_PORT,
+  dialect: 'mysql',
   logging: true,
 });
 
 // call all the models
-const User = require("../models/user")(sequelize);
-const UserLoginSession = require("../models/userLoginSession")(sequelize);
-const AccountVerificationToken = require("../models/accountVerificationToken")(
-  sequelize
+const User = require('../models/user')(sequelize);
+const UserLoginSession = require('../models/userLoginSession')(sequelize);
+const AccountVerificationToken = require('../models/accountVerificationToken')(
+  sequelize,
 );
-const ForgotPasswordToken = require("../models/forgotPasswordToken")(sequelize);
-const Admin = require("../models/admin")(sequelize);
-const AdminLoginSession = require("../models/adminLoginSession")(sequelize);
-const Alamat = require("../models/alamat")(sequelize);
-const Produk = require("../models/produk")(sequelize);
-const Stok = require("../models/stok")(sequelize);
-const StokStatus = require("../models/stokStatus")(sequelize);
-const PurchaseOrder = require("../models/purchaseOrder")(sequelize);
-const KategoriProduk = require("../models/kategoriProduk")(sequelize);
-const Cart = require("../models/cart")(sequelize);
-const DaftarTransaksi = require("../models/daftarTransaksi")(sequelize);
-const DetailTransaksi = require("../models/detailTransaksi")(sequelize);
-const MetodePembayaran = require("../models/metodePembayaran")(sequelize);
-const BuktiPembayaran = require("../models/buktiPembayaran")(sequelize);
-const StatusTransaksi = require("../models/statusTransaksi")(sequelize);
-const MutasiStok = require("../models/mutasiStok")(sequelize);
-const UserProduct = require("../models/userProduct")(sequelize);
+const ForgotPasswordToken = require('../models/forgotPasswordToken')(sequelize);
+const Admin = require('../models/admin')(sequelize);
+const AdminLoginSession = require('../models/adminLoginSession')(sequelize);
+const Alamat = require('../models/alamat')(sequelize);
+const Produk = require('../models/produk')(sequelize);
+const Stok = require('../models/stok')(sequelize);
+const StokStatus = require('../models/stokStatus')(sequelize);
+const PurchaseOrder = require('../models/purchaseOrder')(sequelize);
+const KategoriProduk = require('../models/kategoriProduk')(sequelize);
+const Cart = require('../models/cart')(sequelize);
+const DaftarTransaksi = require('../models/daftarTransaksi')(sequelize);
+const DetailTransaksi = require('../models/detailTransaksi')(sequelize);
+const MetodePembayaran = require('../models/metodePembayaran')(sequelize);
+const BuktiPembayaran = require('../models/buktiPembayaran')(sequelize);
+const StatusTransaksi = require('../models/statusTransaksi')(sequelize);
+const MutasiStok = require('../models/mutasiStok')(sequelize);
+const UserProduct = require('../models/userProduct')(sequelize);
 
 // defind the relationship of the model
-Admin.hasMany(AdminLoginSession, { foreignKey: "admin_id" });
-AdminLoginSession.belongsTo(Admin, { foreignKey: "admin_id" });
+Admin.hasMany(AdminLoginSession, { foreignKey: 'admin_id' });
+AdminLoginSession.belongsTo(Admin, { foreignKey: 'admin_id' });
 
 User.hasMany(UserLoginSession);
 UserLoginSession.belongsTo(User);
